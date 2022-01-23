f5e1a10257aca474d06013787385e6789c4b91fa
zakaryaboudouar <58151284+zakaryaboudouar@users.noreply.github.com>
2022-01-23 17:48:01 +0100

    ProductsApp

---
+++ src/app/state/product.state.ts
@@ -0,0 +1,27 @@
+
+export enum ProductActionsTypes{
+  GET_ALL_PRODUCTS="[Product] Get All products",
+  GET_SELECTED_PRODUCTS="[Product] Get Selected products",
+  GET_AVAILABLE_PRODUCTS="[Product] Get Available products",
+  SEARCH_PRODUCTS="[Product] Search products",
+  NEW_PRODUCT="[Product] New product",
+  SELECT_PRODUCT="[Product] SElect product",
+  EDIT_PRODUCT="[Product] Edit product",
+  DELETE_PRODUCT="[Product] Delete product",
+}
+
+export interface ActionEvent {
+  type:ProductActionsTypes,
+  payload?:any
+}
+export enum DataStateEnum {
+  LOADING,
+  LOADED,
+  ERROR
+}
+
+export interface AppDataState<T> {
+  dataState:DataStateEnum,
+  data?:T,
+  errorMessage?:string
+}
